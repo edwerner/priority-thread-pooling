a718cd01fa33a2087a9d9fea0140b4e2bc5649e3
Ed Werner <android.devops@gmail.com>
2018-11-13 12:08:41 -0500

    adds scheduler class

---
+++ src/main/java/Scheduler.java
@@ -0,0 +1,69 @@
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.Iterator;
+import java.util.PriorityQueue;
+import java.util.Queue;
+import java.util.Random;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
+
+public class Scheduler {
+
+  final static int THREAD_COUNT = 10;
+  private static ExecutorService executorService;
+
+  public static void main(String[] args) throws InterruptedException, ExecutionException {
+
+    executorService = Executors.newFixedThreadPool(THREAD_COUNT);
+
+    // natural ordering example of priority queue
+    Queue<Integer> integerPriorityQueue = new PriorityQueue<>(THREAD_COUNT);
+    Random rand = new Random();
+    for (int i = 0; i < THREAD_COUNT; i++) {
+      integerPriorityQueue.add(new Integer(rand.nextInt(100)));
+    }
+
+    // PriorityQueue example with Comparator
+    Queue<Task> taskPriorityQueue = new PriorityQueue<>(THREAD_COUNT, idComparator);
+    addTasksToQueue(taskPriorityQueue);
+    pollTasksFromQueue(taskPriorityQueue);
+  }
+
+  // Comparator anonymous class implementation
+  public static Comparator<Task> idComparator = new Comparator<Task>() {
+
+    @Override
+    public int compare(Task t1, Task t2) {
+      return (int) (t2.getId() - t1.getId());
+    }
+  };
+
+  // utility method to add random data to Queue
+  private static void addTasksToQueue(Queue<Task> taskPriorityQueue) {
+    Random rand = new Random();
+    for (int i = 0; i < THREAD_COUNT; i++) {
+      int id = rand.nextInt(100);
+      taskPriorityQueue.add(new Task(id, "Thread_ " + id));
+    }
+  }
+
+  // utility method to poll data from queue
+  private static void pollTasksFromQueue(Queue<Task> taskPriorityQueue)
+      throws InterruptedException, ExecutionException {
+    int counter = 0;
+    while (counter < taskPriorityQueue.size()) {
+      Task task = taskPriorityQueue.poll();
+      executeTask(task.getId());
+      // System.out.println(taskPriorityQueue.size());
+      counter++;
+    }
+  }
+
+  public static void executeTask(int id) throws InterruptedException, ExecutionException {
+    Future<String> future = executorService.submit(() -> "Thread executed with priority " + id);
+    String result = future.get();
+    System.out.println(result);
+  }
+}
