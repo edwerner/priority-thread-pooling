6d6c32ce0005a7eabac0eaa3a96d0824dc473eca
Ed Werner <android.devops@gmail.com>
2018-11-14 12:48:41 -0500

    adds thread pooling

--- src/main/java/Scheduler.java
+++ src/main/java/Scheduler.java
@@ -1,6 +1,15 @@
+import java.util.ArrayList;
+import java.util.List;
 import java.util.concurrent.ExecutionException;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
 
 public class Scheduler {
+
+  private static ExecutorService executorService;
+  private static final int THREAD_COUNT = 10;
+  private static List<TaskThread> taskList = new ArrayList<TaskThread>();
 
   /**
    * Initializes ExecutorService, creates new thread pool, populates integer
@@ -14,28 +23,33 @@ public class Scheduler {
 
 	System.out.println("\n\nCar Initialization Started\n");
 	
+	executorService = Executors.newFixedThreadPool(THREAD_COUNT);
+	
 	//create and start threads
 	TaskThread thermostat = new TaskThread("Thermostat", 50, 1);
-	thermostat.start();
+	taskList.add(thermostat);
 	TaskThread windshieldWipers = new TaskThread("Windshield Wipers", 30,9);
-	windshieldWipers.start();
+    taskList.add(windshieldWipers);
 	TaskThread cruiseControl = new TaskThread("Cruise Control", 20, 6);
-	cruiseControl.start();
+    taskList.add(cruiseControl);
 	TaskThread seatBeltSensor = new TaskThread("Seat Belt Sensor", 27, 7);
-	seatBeltSensor.start();
+    taskList.add(seatBeltSensor);
 	TaskThread gasolineMeter = new TaskThread("Gasoline Meter", 39, 8);
-	gasolineMeter.start();
+    taskList.add(gasolineMeter);
 	TaskThread rpmMeter = new TaskThread("RPM Meter", 42, 8);
-	rpmMeter.start();
+    taskList.add(rpmMeter);
 	TaskThread speedometer = new TaskThread("Speedometer", 20, 10);
-	speedometer.start();
+    taskList.add(speedometer);
 	TaskThread laneAssist = new TaskThread("Lane Assist", 37, 4);
-	laneAssist.start();
+    taskList.add(laneAssist);
 	TaskThread autoBrake = new TaskThread("Auto Brake", 21, 10);
-	autoBrake.start();
+    taskList.add(autoBrake);
 	TaskThread obstacleDetection = new TaskThread("Obstacle Detection", 36, 9);
-	obstacleDetection.start();
-
+    taskList.add(obstacleDetection);
+    
+    for (TaskThread thread : taskList) {
+      executorService.submit(() -> thread.start());
+    }
   }
 
  
